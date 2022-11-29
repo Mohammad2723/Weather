1233557654e9d63ff1907bbc5a0e7748bc2d0a22
mohammad2723 <kotlin2723@gmail.com>
2022-11-29 16:07:49 +0330

    Splash and navigation is implement

--- app/src/main/java/com/github/ebrahimi16153/weather/screens/SplashScreen.kt
+++ app/src/main/java/com/github/ebrahimi16153/weather/screens/SplashScreen.kt
@@ -1,25 +1,53 @@
 package com.github.ebrahimi16153.weather.screens
 
-import android.window.SplashScreen
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.github.ebrahimi16153.weather.R
 import com.github.ebrahimi16153.weather.ui.theme.MyColors
 
 @Composable
-fun SplashScreen(navController: NavController){
-    Scaffold( modifier = Modifier.fillMaxSize()) {
-        Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center, modifier = Modifier.fillMaxSize()) {
-            Text(text = "SPLASH SCREEN " , color = MyColors().text)
+fun SplashScreen(navController: NavController) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    )
+    {
+        Surface(
+            modifier = Modifier
+                .padding(15.dp)
+                .size(330.dp),
+            color = Color.Transparent,
+            shape = CircleShape,
+            border = BorderStroke(width = 2.dp, color = MyColors().text)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+
+                Icon(
+                    painter = painterResource(id = R.drawable.sun),
+                    contentDescription = "",
+                    tint = MyColors().text
+                )
+                Text(text = "Find The Sun?", color = MyColors().text)
+            }
+
 
         }
     }
+
 
 }
