d024d333e8c4c685f538ba3d22197e449bf123c0
mohammad2723 <kotlin2723@gmail.com>
2022-12-07 15:08:35 +0330

    Circle of content work perfectly

--- app/src/main/java/com/github/ebrahimi16153/weather/screens/main/MainScreen.kt
+++ app/src/main/java/com/github/ebrahimi16153/weather/screens/main/MainScreen.kt
@@ -6,16 +6,14 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -25,7 +23,9 @@ import androidx.navigation.NavHostController
 import coil.compose.rememberImagePainter
 import com.github.ebrahimi16153.weather.data.DataOrException
 import com.github.ebrahimi16153.weather.model.Weather
+import com.github.ebrahimi16153.weather.navigation.WeatherScreensName
 import com.github.ebrahimi16153.weather.ui.theme.MyColors
+import com.github.ebrahimi16153.weather.util.formatDate
 import com.github.ebrahimi16153.weather.viewmodel.MainViewModel
 import com.github.ebrahimi16153.weather.widgets.WeatherAppBar
 
@@ -41,15 +41,21 @@ fun MainScreen(navController: NavHostController, viewModel: MainViewModel = hilt
         val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
             initialValue = DataOrException(isLoading = true)
         ) {
-            value = viewModel.getWeatherData(city = "karaj")
+            value = viewModel.getWeatherData(city = "hashtgerd")
         }.value
 
         if (weatherData.isLoading == true) {
             CircularProgressIndicator(color = MyColors().text)
 
         } else if (weatherData.data != null) {
-            
-            MainScaffold(weather = weatherData.data!!,navController = navController)
+
+            MainScaffold(weather = weatherData.data!!, navController = navController)
+        } else {
+            Text(text = "We can't find the city")
+            TryAgain {
+                navController.popBackStack()
+                navController.navigate(WeatherScreensName.MainScreen.name)
+            }
         }
     }
 }
@@ -81,7 +87,9 @@ fun MainScaffold(weather:Weather ,navController: NavController) {
 @Composable
 fun MainContent(weather: Weather) {
     // icon Url
-    val iconUrl = "https://openweathermap.org/img/wn/${weather.list?.get(0)?.weather?.get(0)?.icon}.png"
+    val iconUrl =
+        "https://openweathermap.org/img/wn/${weather.list?.get(0)?.weather?.get(0)?.icon}.png"
+
     // Main Column
     Column(
         modifier = Modifier
@@ -91,19 +99,28 @@ fun MainContent(weather: Weather) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Mon March 21", color = MyColors().text)
+
+        // Top Date
+        Text(
+            text = "${weather.list?.get(0)?.dt?.let { formatDate(it) }}",
+            color = MyColors().text,
+            fontWeight = FontWeight.Bold,
+            fontSize = 14.sp,
+            modifier = Modifier.padding(bottom = 10.dp)
+        )
 
         //Start Circle content
         Surface(
             modifier = Modifier
-                .size(200.dp)
-                .padding(5.dp), shape = CircleShape, color = MyColors().primary
+                .size(200.dp),
+            shape = CircleShape, color = MyColors().primary
         ) {
             Column(
                 modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
+
                 // icon of clouds
                 WeatherIcon(url = iconUrl)
                
@@ -111,18 +128,20 @@ fun MainContent(weather: Weather) {
 
                 // Temp
                 Text(
-                    text = weather.list?.get(0)?.temp?.day.toString() + "°",
+                    text = weather.list?.get(0)?.temp?.day?.toInt().toString() + "°",
                     color = MyColors().onPrimary,
                     fontWeight = FontWeight.ExtraBold,
                     fontSize = 35.sp
                 )
+
                 // Clouds
                 weather.list?.get(0)?.weather?.get(0)?.description?.let {
                     Text(
                         text = it,
                         color = MyColors().onPrimary,
-                        fontSize = 16.sp,
-                        fontWeight = FontWeight.Bold
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Medium,
+                        fontStyle = FontStyle.Italic
                     )
                 }
 
@@ -140,9 +159,19 @@ fun MainContent(weather: Weather) {
 fun WeatherIcon(url: String) {
 
     Image(
+        // coil rememberImagePainter
         painter = rememberImagePainter(url),
         contentDescription = "icon",
-        modifier = Modifier.size(70.dp)
+        modifier = Modifier.size(50.dp)
     )
+
+}
+
+
+@Composable
+fun TryAgain(onClick: () -> Unit) {
+    Button(onClick = onClick) {
+        Text(text = "TryAgain")
+    }
 
 }
