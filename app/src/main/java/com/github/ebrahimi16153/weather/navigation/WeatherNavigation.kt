760179788f9fc1fccf409190749203ff39c81f65
mohammad2723 <kotlin2723@gmail.com>
2022-12-12 12:59:54 +0330

    SearchBarScreen work

--- app/src/main/java/com/github/ebrahimi16153/weather/navigation/WeatherNavigation.kt
+++ app/src/main/java/com/github/ebrahimi16153/weather/navigation/WeatherNavigation.kt
@@ -1,14 +1,19 @@
 package com.github.ebrahimi16153.weather.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.github.ebrahimi16153.weather.screens.SplashScreen
 import com.github.ebrahimi16153.weather.screens.main.MainScreen
+import com.github.ebrahimi16153.weather.screens.search.SearchScreen
 import com.github.ebrahimi16153.weather.viewmodel.MainViewModel
 
+@ExperimentalComposeUiApi
 @Composable
 fun WeatherNavigation() {
     val navController = rememberNavController()
@@ -20,10 +25,26 @@ fun WeatherNavigation() {
         composable(WeatherScreensName.SplashScreen.name) {
             SplashScreen(navController = navController)
         }
+        // Main Screen
+        //www.google.com/cityname="seattle"
 
-        composable(WeatherScreensName.MainScreen.name) {
-            val mainViewModel = hiltViewModel<MainViewModel>()
-            MainScreen(navController = navController , mainViewModel)
+        val route = WeatherScreensName.MainScreen.name
+
+        composable("$route/{city}", arguments = listOf(navArgument(name = "city") {
+            type = NavType.StringType })) { navBack ->
+
+            navBack.arguments?.getString("city").let { city ->
+
+                val mainViewModel = hiltViewModel<MainViewModel>()
+                MainScreen(navController = navController, mainViewModel, city = city)
+            }
+
+
+        }
+
+        //search Screen
+        composable(WeatherScreensName.SearchScreen.name) {
+            SearchScreen(navController = navController)
         }
 
 
